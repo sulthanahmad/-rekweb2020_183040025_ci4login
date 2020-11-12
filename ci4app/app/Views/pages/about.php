b28ba031898988375d79c758d2fadb159689f7ab
sulthanahmad <sulthanahmad255@gmail.com>
2020-11-12 11:40:46 +0700

    ci4login

---
+++ ci4app/app/Views/pages/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quibusdam officiis iste inventore incidunt earum voluptatibus rerum porro, nesciunt animi veritatis quis et qui eaque odio modi quam doloribus in!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
